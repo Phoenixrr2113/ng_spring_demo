5f3538e3b8b995c20f92c2ce15f1eec437d1e4c1
Randy Wilson <contact@randywilson.io>
2020-03-07 06:21:34 -0500

    init setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'lidp-test';
+  title = "lidp-test";
+  isChecked = true;
+
+  constructor() {
+    this.title = "new person";
+  }
+
+  onChange($event) {
+    console.log("changes", $event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
